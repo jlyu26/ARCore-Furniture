fb68bb43194bea6c05d650060a43e4df391ca77f
Jinyan.LYU <jlyu@wpi.edu>
2018-09-05 09:42:47 -0400

    Edit model specs, add tap furniture to sceneform function.

--- app/src/main/java/edu/wpi/jlyu/sceneformfurniture/MainActivity.java
+++ app/src/main/java/edu/wpi/jlyu/sceneformfurniture/MainActivity.java
@@ -1,21 +1,27 @@
 package edu.wpi.jlyu.sceneformfurniture;
 
+import android.app.AlertDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArFragment fragment;
 
-    private String selectedObject;
+    private Uri selectedObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
         fragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    Log.d("model", selectedObject);
+                     if (plane.getType() != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                         return;
+                     }
+
+                     Anchor anchor = hitResult.createAnchor();
+
+                     placeObject(fragment, anchor, selectedObject);
                 }
         );
     }
@@ -39,25 +51,55 @@ public class MainActivity extends AppCompatActivity {
         ImageView chair = new ImageView( this );
         chair.setImageResource(R.drawable.chair_thumb);
         chair.setContentDescription("chair");
-        chair.setOnClickListener(view -> {selectedObject = "chair";});
+        chair.setOnClickListener(view -> {selectedObject = Uri.parse("chair.sfb");});
         gallery.addView(chair);
 
         ImageView lamp = new ImageView( this );
         lamp.setImageResource(R.drawable.lamp_thumb);
         lamp.setContentDescription("lamp");
-        lamp.setOnClickListener(view -> {selectedObject = "lamp";});
+        lamp.setOnClickListener(view -> {selectedObject = Uri.parse("lamp.sfb");});
         gallery.addView(lamp);
 
         ImageView sofa = new ImageView( this );
         sofa.setImageResource(R.drawable.sofa_thumb);
         sofa.setContentDescription("sofa");
-        sofa.setOnClickListener(view -> {selectedObject = "sofa";});
+        sofa.setOnClickListener(view -> {selectedObject = Uri.parse("sofa.sfb");});
         gallery.addView(sofa);
 
         ImageView table = new ImageView( this );
         table.setImageResource(R.drawable.table_thumb);
         table.setContentDescription("table");
-        table.setOnClickListener(view -> {selectedObject = "table";});
+        table.setOnClickListener(view -> {selectedObject = Uri.parse("table.sfb");});
         gallery.addView(table);
     }
+
+    // placeObject() takes the position and orientation of an object to be rendered as
+    // an anchor and starts async loading the object. Once the builder built the object,
+    // addNodeToScene() will be called, and that will actually create the object in
+    // the camera view.
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
+        ModelRenderable.builder()
+                .setSource(fragment.getContext(), model)
+                .build()
+                .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                .exceptionally((throwable -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder( this );
+                    builder.setMessage(throwable.getMessage())
+                            .setTitle("Error!");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return null;
+                }));
+    }
+
+    // Place the object on AR
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
 }
