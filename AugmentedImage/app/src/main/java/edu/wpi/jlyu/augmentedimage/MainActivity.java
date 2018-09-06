91dd793e8f2f1b696d9370b051e4f7a2ed001d04
Jinyan.LYU <jlyu@wpi.edu>
2018-09-06 17:03:22 -0400

    Add AR image function.

--- AugmentedImage/app/src/main/java/edu/wpi/jlyu/augmentedimage/MainActivity.java
+++ AugmentedImage/app/src/main/java/edu/wpi/jlyu/augmentedimage/MainActivity.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.AugmentedImageDatabase;
 import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
 import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
@@ -20,10 +24,12 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomArFragment arFragment;
+    private boolean shouldAddModel = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
         arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getArSceneView().getScene().setOnUpdateListener(this::onUpdateFrame);
     }
 
     public boolean setupAugmentedImageDb(Config config, Session session) {
@@ -58,6 +65,23 @@ public class MainActivity extends AppCompatActivity {
             Log.e("ImageLoad", "IO Exception while loading: ", e);
         }
         return null;
+    }
+
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
+
+        for (AugmentedImage augmentedImage: augmentedImages) {
+            if (augmentedImage.getTrackingState() == TrackingState.TRACKING) {
+                if (augmentedImage.getName().equals("mug") && shouldAddModel) {
+                    placeObject(arFragment,
+                            augmentedImage.createAnchor(augmentedImage.getCenterPose()),
+                            Uri.parse("Cup.sfb"));
+                    shouldAddModel = false;
+                }
+            }
+        }
     }
 
     private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
